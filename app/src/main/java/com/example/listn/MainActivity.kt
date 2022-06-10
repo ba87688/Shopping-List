1fecdc18ab105898f1c60c06f25d33b35ab18104
Jagi <ba87688@gmail.com>
2022-06-10 14:30:16 -0500

    database classes

--- app/src/main/java/com/example/listn/MainActivity.kt
+++ app/src/main/java/com/example/listn/MainActivity.kt
@@ -6,8 +6,8 @@ import com.example.listn.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-
     private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
